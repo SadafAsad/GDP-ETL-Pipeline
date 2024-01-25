2b4ab02fc22daa01073b7d88da9d70f6bde8f7b9
SadafAsad <34832185+SadafAsad@users.noreply.github.com>
2024-01-25 11:32:35 -0500

    testing

--- etl_ops.py
+++ etl_ops.py
@@ -58,3 +58,33 @@ table_attribs = ['Country', 'GDP_USD_millions']
 db_name = 'World_Economies.db'
 table_name = 'Countries_by_GDP'
 csv_path = './Countries_by_GDP.csv'
+
+# # -------- RUNNING THE PROCESS ----------
+# log_progress('Preliminaries complete. Initiating ETL process')
+
+# df = extract(url, table_attribs)
+
+# log_progress('Data extraction complete. Initiating Transformation process')
+
+# df = transform(df)
+
+# log_progress('Data transformation complete. Initiating loading process')
+
+# load_to_csv(df, csv_path)
+
+# log_progress('Data saved to CSV file')
+
+# sql_connection = sqlite3.connect(db_name)
+
+# log_progress('SQL Connection initiated.')
+
+# load_to_db(df, sql_connection, table_name)
+
+# log_progress('Data loaded to Database as table. Running the query')
+
+# query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+# run_query(query_statement, sql_connection)
+
+# log_progress('Process Complete.')
+
+# sql_connection.close()
