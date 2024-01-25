ec699b8b51f68acf1273765079b96513747e99af
SadafAsad <34832185+SadafAsad@users.noreply.github.com>
2024-01-25 11:22:46 -0500

    imports

---
+++ etl_ops.py
@@ -0,0 +1,11 @@
+# ETL operations on Country-GDP data
+
+# import section
+import requests
+import sqlite3
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
