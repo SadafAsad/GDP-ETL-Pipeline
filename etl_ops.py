da28b01d880bcc843b53a285736816b13bc7966d
SadafAsad <34832185+SadafAsad@users.noreply.github.com>
2024-01-25 11:31:53 -0500

    required entities

--- etl_ops.py
+++ etl_ops.py
@@ -52,3 +52,9 @@ def log_progress(message):
     timestamp = now.strftime(timestamp_format) 
     with open("./etl_project_log.txt","a") as f: 
         f.write(timestamp + ',' + message + '\n')
+
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs = ['Country', 'GDP_USD_millions']
+db_name = 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path = './Countries_by_GDP.csv'
