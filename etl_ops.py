f9e505c0e9ddf80afacd14503bed1fa82363bda7
SadafAsad <34832185+SadafAsad@users.noreply.github.com>
2024-01-25 11:25:34 -0500

    extract

--- etl_ops.py
+++ etl_ops.py
@@ -8,4 +8,19 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-
+def extract(url, table_attribs):
+    # Scraping data and storing to a dataframe
+    html_page = requests.get(url).text
+    data = BeautifulSoup(html_page, 'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables = data.find_all('tbody')
+    rows = tables[2].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0 :
+            if col[0].find('a') is not None and '—' not in col[2]:
+                data_dict = {"Country": col[0].a.contents[0],
+                            "GDP_USD_millions": col[2].contents[0]}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
+    return df
